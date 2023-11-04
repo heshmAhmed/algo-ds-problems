1d64dde6fd2708c8b7fd5f443fb56ff04a23d548
Hesham Ahmed <45402176+heshmAhmed@users.noreply.github.com>
2023-11-04 03:52:25 +0200

    Update 0100-same-tree.java

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -14,13 +14,11 @@
  * }
  */
 class Solution {
-    ArrayList<Integer> tree1 = new ArrayList<>();
-    ArrayList<Integer> tree2 = new ArrayList<>();
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p == null && q == null)
-            return true;
-        else if((p == null && q != null) || (p != null && q == null) || (p.val != q.val))
-            return false;
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-    }
+            if(p == null && q == null)
+                return true;
+            else if(p == null || q == null || p.val != q.val)
+                return false;
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
 }
